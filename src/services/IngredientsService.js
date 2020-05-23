acb80f793f8225c52ea918268392b1006344ceac
Clarisa <clarisa@flashpack.com>
2020-05-23 19:18:21 +0200

    Setup project and create search component

---
+++ src/services/IngredientsService.js
@@ -0,0 +1,7 @@
+import ingredients from './ingredients.json'
+
+export default {
+    getIngredients() {
+        return ingredients
+    }
+}
---
+++ src/services/RecipesService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: `http://localhost:3000`,
+    //baseURL: `https://api.spoonacular.com`,
+    withCredentials: false, // This is the default
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+    // query
+    //https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=d4308995a2ae43578da556983599118e
+})
+
+export default {
+    getEvents() {
+        return apiClient.get('/recipes/')
+    }
+}
